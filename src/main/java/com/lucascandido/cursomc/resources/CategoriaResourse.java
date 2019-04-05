6260e20928822fd0ff7c98f981d289a6b62b3231
Lucas Candido <lucascandido.ti@gmail.com>
2019-04-05 14:52:12 -0300

    Testanto a primeira classe de Dominio

--- src/main/java/com/lucascandido/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/lucascandido/cursomc/resources/CategoriaResourse.java
@@ -1,15 +1,31 @@
 package com.lucascandido.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucascandido.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourse {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 	}
+	
 }
