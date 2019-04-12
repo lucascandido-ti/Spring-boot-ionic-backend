8a2a794b1db3e1e7c55788454988bcefc5cacaad
lucascandido-ti <lucascandido.ti@gmail.com>
2019-04-12 12:08:39 -0300

    Criando profile de test

---
+++ src/main/java/com/lucascandido/cursomc/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.lucascandido.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucascandido.cursomc.services.DBServices;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBServices dbServices;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbServices.instantiateTestDatabase();
+		
+		return true;
+	}
+}
