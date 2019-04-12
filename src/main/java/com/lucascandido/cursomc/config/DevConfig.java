9e82f68d94519f9d48ba0b8ae657d0e10d644ef3
lucascandido-ti <lucascandido.ti@gmail.com>
2019-04-12 12:50:51 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/lucascandido/cursomc/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.lucascandido.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucascandido.cursomc.services.DBServices;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBServices dbServices;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbServices.instantiateTestDatabase();
+		return true;
+		
+	}
+}
