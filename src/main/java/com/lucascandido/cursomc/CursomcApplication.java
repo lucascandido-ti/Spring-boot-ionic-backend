1169a574cc6ebfc048654fe09aec1808744a131a
lucascandido-ti <lucascandido.ti@gmail.com>
2019-04-16 14:58:13 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/lucascandido/cursomc/CursomcApplication.java
+++ src/main/java/com/lucascandido/cursomc/CursomcApplication.java
@@ -1,14 +1,18 @@
 package com.lucascandido.cursomc;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucascandido.cursomc.services.S3Services;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Services s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -17,7 +21,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		s3Service.uploadFile("C:\\Users\\Lucas Candido\\Documents\\STS\\projects\\fotos\\firstblackrole.png");
 		
 	}
 
