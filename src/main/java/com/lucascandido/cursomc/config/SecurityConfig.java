077f9f3f66d7399fc0b99aaf1a107a8cbbd2a8ca
lucascandido-ti <lucascandido.ti@gmail.com>
2019-04-22 12:39:06 -0300

    Acrescentando mais produtos para testar infinity scroll

--- src/main/java/com/lucascandido/cursomc/config/SecurityConfig.java
+++ src/main/java/com/lucascandido/cursomc/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll()
 				.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
--- src/main/java/com/lucascandido/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/lucascandido/cursomc/resources/exception/ValidationError.java
@@ -8,7 +8,6 @@ public class ValidationError extends StandardError {
 	
 	private List<FieldMessage> errors = new ArrayList<>();
 
-	
 
 	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
 		super(timestamp, status, error, message, path);
